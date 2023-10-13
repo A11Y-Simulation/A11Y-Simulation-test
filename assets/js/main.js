f4fba9bacb432dd2f956c64f7b3d4a802d1f1a46
moufid-elaroui <moufid.elaroui@gmail.com>
2023-10-13 16:07:58 +0200

    test final 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
-// let mobileNavBtn = document.getElementById("mobile-nav-btn");
-// let mobileNav = document.getElementById("mobile-nav");
+let mobileNavBtn = document.getElementById("mobile-nav-btn");
+let mobileNav = document.getElementById("mobile-nav");
 
-// mobileNavBtn.addEventListener("click", function () {
-//   if (mobileNav.hasAttribute("class")) {
-//     mobileNav.removeAttribute("class");
-//   } else {
-//     mobileNav.setAttribute("class", "active");
-//   }
-// });
+mobileNavBtn.addEventListener("click", function () {
+  if (mobileNav.hasAttribute("class")) {
+    mobileNav.removeAttribute("class");
+  } else {
+    mobileNav.setAttribute("class", "active");
+  }
+});
